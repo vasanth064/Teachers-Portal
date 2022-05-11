9e0dd62821fdc99bcb6f67e1680dc957085babf0
vasanth064 <vasanthvdev@gmail.com>
2022-05-11 10:03:12 +0530

    added FirestoreContex API

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.js';
 import AuthenticationProvider from './Context/AuthContext.js';
+import FirestoreProvider from './Context/FirestoreContex.js';
 
 ReactDOM.render(
   <BrowserRouter>
     <AuthenticationProvider>
-      <App />
+      <FirestoreProvider>
+        <App />
+      </FirestoreProvider>
     </AuthenticationProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker
+//     .register(`${process.env.PUBLIC_URL}/service-worker.js`)
+//     .catch((err) => console.log('Error 💥', err));
+// }
