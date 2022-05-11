895181a4151867a7ece0149d7087a7bcb80535cc
vasanth064 <vasanthvdev@gmail.com>
2022-05-11 10:21:41 +0530

    added firestore Context

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// import { getStorage } from 'firebase/storage';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -15,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// export const storageBucket = getStorage(app);
+export const storageBucket = getStorage(app);
