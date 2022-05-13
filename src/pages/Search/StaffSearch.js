57dfa31257c47212d2875d107316ae99b9f30975
Vasanth V V <vasanthvdev@gmail.com>
2022-05-13 14:13:03 +0530

    added search and sorting options

--- src/pages/Search/StaffSearch.js
+++ src/pages/Search/StaffSearch.js
@@ -7,45 +7,68 @@ import { debounce } from 'lodash';
 import SearchBar from '../../components/css/SearchBar';
 import SearchResultsContainer from '../../components/SearchResultsContainer';
 import { useFirestore } from '../../Context/FirestoreContext';
+import FormContainer from '../../components/FormContainer';
+import FormOptions from '../../components/FormOptions';
 
 const Staffsearch = () => {
   const { getData } = useFirestore();
   const [teachers, setTeachers] = useState([]);
   const [searchName, setSearchName] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [activeFormOption, setActiveFormOpton] = useState(0);
+
   useEffect(() => {
     (async () => {
       const data = await getData('teachers');
       setTeachers(data);
     })();
   }, []);
+
   const searchTeachers = (search) => {
     debounce(() => setSearchName(search), 500);
-
     const newResults = teachers.filter((teacher) => {
       if (search === '') {
         return teacher;
       }
-
-      return teacher.name.toLowerCase().includes(search.toLowerCase());
+      if (activeFormOption === 0) {
+        return teacher.name.toLowerCase().includes(search.toLowerCase());
+      }
+      if (activeFormOption === 1) {
+        return teacher.department.toLowerCase().includes(search.toLowerCase());
+      }
+      if (activeFormOption === 2) {
+        return teacher.staffID.toLowerCase().includes(search.toLowerCase());
+      }
     });
-
     setSearchResults(newResults);
-
     if (search === '') {
       setSearchResults([]);
     }
   };
+
+  const handleActiveFormOption = (index) => setActiveFormOpton(index);
+
   return (
     <div>
       <PageHeader text='Staff Search' />
       <PageContent>
-        <SearchBar
-          icon={<BiSearch size='3rem' />}
-          name='teacherName'
-          placeholder='Teachers Search'
-          onChange={(e) => searchTeachers(e.target.value)}
-        />
+        <form autoComplete='off'>
+          <FormContainer>
+            <SearchBar
+              icon={<BiSearch size='3rem' />}
+              name='teacherName'
+              placeholder='Teachers Search'
+              onChange={(e) => searchTeachers(e.target.value)}
+            />
+            <div style={{ display: 'flex', gap: '1rem' }}>
+              <FormOptions
+                data={['name', 'department', 'staff ID']}
+                activeFormOption={activeFormOption}
+                handleActiveFormOption={handleActiveFormOption}
+              />
+            </div>
+          </FormContainer>
+        </form>
 
         <SearchResultsContainer>
           {searchResults.length === 0
