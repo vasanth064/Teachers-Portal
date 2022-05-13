52b886b06840073c20644f7f427dca0a140a1be9
rajkumar035 <kishorekumardhandapani71@gmail.com>
2022-05-13 16:12:54 +0530

    Added Attendance Report

--- src/pages/Attendance/AttendanceReport.js
+++ src/pages/Attendance/AttendanceReport.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import PageContent from '../../components/PageContent'
-import PageHeader from '../../components/PageHeader'
+import React from 'react';
+import PageContent from '../../components/PageContent';
+import PageHeader from '../../components/PageHeader';
+import Table from '../../components/Table';
+import styled from 'styled-components';
+import GreenButton from '../../components/GreenButton';
+
+const ContentBody = styled.div`
+  text-align: center;
+`;
 
 const AttendanceReport = () => {
   return (
     <div>
-        <PageHeader text='Attendance Report'/>
-        <PageContent>Attendance Report</PageContent>
+      <PageHeader text='Attendance Report' />
+      <PageContent>
+        <ContentBody>
+          <Table />
+          <GreenButton style={{ color: 'white', margin: '4rem' }}>
+            Get Report
+          </GreenButton>
+        </ContentBody>
+      </PageContent>
     </div>
-  )
-}
+  );
+};
 
-export default AttendanceReport
+export default AttendanceReport;
